7b6e34f9159446ebf79fea2232c32bce609b9144
Will Liao <wyliao76@gmail.com>
2023-10-10 10:20:14 -0700

    wip

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@ function sayHello() {
     
 }
 //sayHello();
+
+var login = false
+const loginFunction = () => {
+    console.log(login)
+    login = true
+    console.log(login)
+    alert("redirect in 1 sec...")
+    window.setInterval(() => {
+        return window.location.href = "http://localhost:3000/main.html";
+    }, 1000)
+}
