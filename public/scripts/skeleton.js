a0d9a4c0fa1571006d306269074eec45468766f6
Will Liao <wyliao76@gmail.com>
2023-11-01 16:14:17 -0700

    fix footer auth issue

--- public/scripts/skeleton.js
+++ public/scripts/skeleton.js
@@ -1,17 +1,15 @@
 function loadSkeleton() {
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) { // if the pointer to "user" object is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
-            console.log($('.navplaceholder').load('./text/nav_after_login.html'));
-            console.log($('.footerplaceholder').load('./text/footer.html'));
+            console.log($('#navplaceholder').load('./text/navbar.html'))
+            console.log($('#footerplaceholder').load('./text/footer.html'))
         } else {
             // No user is signed in.
-            console.log($('.navplaceholder').load('./text/navbar.html'));
-            console.log($('.footerplaceholder').load('./text/footerbeforelogin.html'));
+            console.log($('#navplaceholder').load('./text/navbar.html'))
+            console.log($('#footerplaceholder').load('./text/footerbeforelogin.html'))
         }
-    });
-    
+    })
 }
-loadSkeleton(); //invoke the function
+loadSkeleton() // invoke the function
