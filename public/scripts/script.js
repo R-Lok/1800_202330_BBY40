d11b4f6694a899132c5a4046c08245e191556a12
Will Liao <wyliao76@gmail.com>
2023-11-05 12:56:27 -0800

    add theme prototype

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -12,7 +12,17 @@ function redirectToIndex() {
     window.location.href = './index.html'
 }
 
-function toggleDarkMode() {
-    const element = document.body
-    element.classList.toggle('dark-mode')
+const saveTheme = (theme) => {
+    localStorage.setItem('theme', theme)
+    applyTheme()
 }
+
+const applyTheme = () => {
+    if (localStorage.getItem('theme') === 'light') {
+        document.head.appendChild(document.createElement('style')).innerHTML = `body > div {background-color: var(--main-color4);color: var(--main-color5);}`
+    } else {
+        document.head.appendChild(document.createElement('style')).innerHTML = `body > div {background-color: var(--main-color5);color: var(--main-color4);}`
+    }
+}
+
+applyTheme()
