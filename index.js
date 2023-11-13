42263cb5439688d1d0057e9b76b8b932f8842fb7
Will Liao <wyliao76@gmail.com>
2023-11-13 15:41:06 -0800

    add security measurement limit size of body

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-const path = require('path')
+app.use(express.json({ limit: '50mb' }))
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.get('/health', (req, res) => {
