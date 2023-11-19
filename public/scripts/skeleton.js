7f3095bf2dd7f1eb4e895f99e3a8371a45ef73b2
Joe Lin <jlin249@my.bcit.ca>
2023-11-19 14:14:24 -0800

    added icon colour change function to skeleton.js and edited loadSkeleton function

--- public/scripts/skeleton.js
+++ public/scripts/skeleton.js
@@ -1,15 +1,41 @@
 function loadSkeleton() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) { // if the pointer to "user" object is not null, then someone is logged in
-            // User is signed in.
-            // Do something for the user here.
-            console.log($('#navplaceholder').load('./text/navbar.html'))
-            console.log($('#footerplaceholder').load('./text/footer.html'))
-        } else {
-            // No user is signed in.
-            console.log($('#navplaceholder').load('./text/navbarbeforelogin.html'))
-            console.log($('#footerplaceholder').load('./text/footerbeforelogin.html'))
-        }
-    })
+    return new Promise((resolve, reject) => {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                // Load content for the user here.
+                $('#navplaceholder').load('./text/navbar.html', function() {
+                    $('#footerplaceholder').load('./text/footer.html', resolve);
+                });
+            } else {
+                // No user is signed in.
+                $('#navplaceholder').load('./text/navbarbeforelogin.html', function() {
+                    $('#footerplaceholder').load('./text/footerbeforelogin.html', resolve);
+                });
+            }
+        });
+    });
 }
-loadSkeleton() // invoke the function
+
+async function blueOut() {
+    await loadSkeleton(); // Wait for loadSkeleton() to complete before proceeding
+    
+    const page = window.location.href;
+    if (new RegExp('.*main.html').test(page)){
+        document.getElementById("home")
+        .setAttribute("style", "filter: invert(25%) sepia(71%) saturate(1573%) hue-rotate(184deg) brightness(93%) contrast(91%);");
+    } else if (new RegExp('.*water_usage.html').test(page)){
+        document.getElementById('usage')
+        .setAttribute("style", "filter: invert(25%) sepia(71%) saturate(1573%) hue-rotate(184deg) brightness(93%) contrast(91%);");
+    } else if (new RegExp('.*waterbill.html').test(page)){
+        document.getElementById('waterbill')
+        .setAttribute("style", "filter: invert(25%) sepia(71%) saturate(1573%) hue-rotate(184deg) brightness(93%) contrast(91%);");
+    } else if (new RegExp('.*setting').test(page)){
+        document.getElementById('settings')
+        .setAttribute("style", "filter: invert(25%) sepia(71%) saturate(1573%) hue-rotate(184deg) brightness(93%) contrast(91%);");
+    }
+}
+
+loadSkeleton().then(blueOut);
+
+
