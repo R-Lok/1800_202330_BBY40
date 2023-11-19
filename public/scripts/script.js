d0e6705779453b370b349595c7e5594038986851
Ryan Lok <rlok.pc@gmail.com>
2023-11-19 01:53:09 -0800

    Moved some functions associated with settings page into settings.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,51 +1,3 @@
-function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log('logging out user')
-        localStorage.clear()
-        redirectToIndex()
-    }).catch((error) => {
-        // An error happened.
-    })
-}
-
-const themeToggle = document.getElementById('theme')
-const measurementSystemToggle = document.getElementById('system')
-
-themeToggle.addEventListener('change', e => updateUserPreferences())
-measurementSystemToggle.addEventListener('change', e => updateUserPreferences())
-
-function updateUserPreferences() {
-    const userId = localStorage.getItem('userId')
-    let currentTheme = getCurrentTheme()
-    let currentMeasurementSystem = getCurrentMeasurementSystem()
-
-    db.collection('users').doc(userId).update({
-        theme: currentTheme,
-        measurement: currentMeasurementSystem
-    })
-}
-
-function getCurrentTheme() {
-    let darkToggled = localStorage.getItem('theme')
-
-    if (darkToggled === 'true') {
-        return 'dark'
-    } else {
-        return 'light'
-    }
-}
-
-function getCurrentMeasurementSystem() {
-    let imperialToggled = localStorage.getItem('system')
-
-    if (imperialToggled === 'true') {
-        return 'imperial'
-    } else {
-        return 'metric'
-    }
-}
-
 function checkIfLoggedIn() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (!user) {
@@ -58,19 +10,6 @@ function redirectToIndex() {
     window.location.href = './index.html'
 }
 
-const setToggle = (ids) => {
-    for (const id of ids) {
-        if (localStorage.getItem(id) === 'true' && document.getElementById(id)) {
-            document.getElementById(id).checked = true
-        }
-    }
-}
-
-const saveToggle = (id) => {
-    localStorage.setItem(id, document.getElementById(id).checked)
-    applyTheme()
-}
-
 const applyTheme = () => {
     if (localStorage.getItem('theme') === 'false') {
         document.getElementById('main_css').href = './styles/style.css'
@@ -80,7 +19,6 @@ const applyTheme = () => {
 }
 
 checkIfLoggedIn()
-setToggle(['system', 'theme'])
 applyTheme()
 
 /* Icon grey out, currently not working
